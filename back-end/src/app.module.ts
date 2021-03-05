d7ee3744cf6f6c80640f91b1660ba4699e78ddc0
alexsandrbarabash <sbarabas176@gmail.com>
2021-03-05 18:27:56 +0200

    add ormconfig.ts

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ormconfig } from '../ormconfig';
+
+/*
+* {
+      type: 'postgres',
+      host: '34.89.218.42',
+      port: 5432,
+      username: 'cfif',
+      password: 'cfif',
+      database: 'cfif',
+      autoLoadEntities: true,
+      synchronize: true,
+    }*/
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot()],
+  imports: [AuthModule, TypeOrmModule.forRoot(ormconfig)],
 })
 export class AppModule {}
