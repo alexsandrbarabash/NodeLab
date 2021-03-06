232fdee0c7e33df8789ffb6d4f7b56ab4675b588
alexsandrbarabash <sbarabas176@gmail.com>
2021-03-06 03:17:22 +0200

    refactoring

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -7,17 +7,23 @@ import { Response, Request } from 'express';
 export class AuthController {
   constructor(private readonly AuthService: AuthService) {}
 
+  private setCookie(res: Response, refreshToken: string) {
+    res.cookie('refresh', refreshToken, {
+      maxAge: 90000000000,
+      httpOnly: true,
+    });
+  }
+
   @Post('register')
   async register(
     @Body() userRegisterLoginDto: UserRegisterLoginDto,
     @Res({ passthrough: true }) response: Response,
   ): Promise<string> {
-    const tokens = await this.AuthService.register(userRegisterLoginDto);
-    response.cookie('refresh', tokens.refreshToken, {
-      maxAge: 90000000000,
-      httpOnly: true,
-    });
-    return tokens.accessToken;
+    const { refreshToken, accessToken } = await this.AuthService.register(
+      userRegisterLoginDto,
+    );
+    this.setCookie(response, refreshToken);
+    return accessToken;
   }
 
   @Post('login')
@@ -25,12 +31,11 @@ export class AuthController {
     @Body() userRegisterLoginDto: UserRegisterLoginDto,
     @Res({ passthrough: true }) response: Response,
   ): Promise<string> {
-    const tokens = await this.AuthService.login(userRegisterLoginDto);
-    response.cookie('refresh', tokens.refreshToken, {
-      maxAge: 90000000000,
-      httpOnly: true,
-    });
-    return tokens.accessToken;
+    const { refreshToken, accessToken } = await this.AuthService.login(
+      userRegisterLoginDto,
+    );
+    this.setCookie(response, refreshToken);
+    return accessToken;
   }
 
   @Get('refresh-tokens')
@@ -38,15 +43,11 @@ export class AuthController {
     @Req() request: Request,
     @Res({ passthrough: true }) response: Response,
   ): Promise<string> {
-    const tokens = await this.AuthService.refreshToken(
+    const { refreshToken, accessToken } = await this.AuthService.refreshToken(
       request.cookies['refresh'],
     );
 
-    response.cookie('refresh', tokens.refreshToken, {
-      maxAge: 90000000000,
-      httpOnly: true,
-    });
-
-    return tokens.accessToken;
+    this.setCookie(response, refreshToken);
+    return accessToken;
   }
 }
