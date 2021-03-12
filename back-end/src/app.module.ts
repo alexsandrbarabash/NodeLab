e40fce0afd6633069d2a95eb5dfbb47d24478a10
alexsandrbarabash <sbarabas176@gmail.com>
2021-03-12 14:06:44 +0200

    added feed module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,8 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ormconfig } from '../ormconfig';
 import { ProfileModule } from './profile/profile.module';
+import { FeedModule } from './feed/feed.module';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot(ormconfig), ProfileModule],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot(ormconfig),
+    ProfileModule,
+    FeedModule,
+  ],
 })
 export class AppModule {}
