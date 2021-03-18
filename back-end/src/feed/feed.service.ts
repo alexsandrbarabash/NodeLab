c82bd5d8b8185db228bebb94cf5b9724c84addf0
Artur24327 <57686461+Artur24327@users.noreply.github.com>
2021-03-18 21:52:58 +0200

    Refactory

--- back-end/src/feed/feed.service.ts
+++ back-end/src/feed/feed.service.ts
@@ -1,35 +1,29 @@
 import { Injectable, Options } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
 import { CreateFeedDto } from './dto/create-feed.dto';
-import { Feed, FeedDocument } from './schemas/feed.chema';
-import { Model } from 'mongoose';
 import { UpdateFeedDto } from './dto/update-feed.dto';
 
 @Injectable()
 export class FeedService {
     
-    constructor(@InjectModel(Feed.name) private feedModel: Model<FeedDocument>) {
+
+    async getAll(): Promise<string> {
+        return 'Ok'
     }
 
-    async getAll(): Promise<Feed[]> {
-        return this.feedModel.find().exec()
+    async getById(id:string): Promise<string>{
+        return 'Ok'
     }
 
-    async getById(id:string): Promise<Feed>{
-        return this.feedModel.findById(id)
-    }
-
-    async create(feedDto: CreateFeedDto): Promise<Feed> { 
-        const newFeed = new this.feedModel(feedDto)
-        return newFeed.save()
+    async create(feedDto: CreateFeedDto): Promise<string> { 
+        return 'Ok'
         }
     
-    async remove(id: string): Promise<Feed>{
-        return this.feedModel.findByIdAndRemove(id)
+    async remove(id: string): Promise<string>{
+        return 'Ok'
     }
 
-    async update(id: string, feedDto: UpdateFeedDto): Promise<Feed>{
-        return this.feedModel.findByIdAndUpdate(id, feedDto, {new: true})
+    async update(id: string, feedDto: UpdateFeedDto): Promise<string>{
+        return 'Ok'
     }
 
 }
