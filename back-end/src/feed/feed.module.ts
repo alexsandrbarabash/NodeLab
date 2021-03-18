3138dcd9c0ee24102fcc2130777a0eb329a5751f
Artur24327 <57686461+Artur24327@users.noreply.github.com>
2021-03-18 20:31:06 +0200

    Commit again
    Dziuubak

--- back-end/src/feed/feed.module.ts
+++ back-end/src/feed/feed.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { FeedService } from './feed.service';
 import { FeedController } from './feed.controller';
-import { Feed, FeedSchema } from './schemas/feed.chema';
+
 
 @Module({
   providers: [FeedService],
   controllers: [FeedController],
-  imports: [MongooseModule.forFeature([
-    {name: Feed.name, schema: FeedSchema}
-  ])]
+  imports: []
 })
 
 export class FeedModule {
